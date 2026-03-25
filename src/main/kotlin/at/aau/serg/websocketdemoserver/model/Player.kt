e4ba3773873fce11ba65e57e9127ee1b5f291d16
Schuster Alwin (IT OS IC VCP HICP) <alwin.schuster@infineon.com>
2026-03-25 20:49:11 +0100

    #5 Fix JSON serialize issue

--- src/main/kotlin/at/aau/serg/websocketdemoserver/model/Player.kt
+++ src/main/kotlin/at/aau/serg/websocketdemoserver/model/Player.kt
@@ -1,14 +1,17 @@
 package at.aau.serg.websocketdemoserver.model
 
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+
 data class Player(
     val id: String,
     var name: String,
-    var position: Int = 0,
-    var money: Int = 1500,
-    var inJail: Boolean = false,
-    var jailTurns: Int = 0,
-    var getOutOfJailCards: Int = 0,
-    val ownedPropertyIds: MutableList<Int> = mutableListOf()
+    @JsonSetter(nulls = Nulls.SKIP) var position: Int = 0,
+    @JsonSetter(nulls = Nulls.SKIP) var money: Int = 1500,
+    @JsonSetter(nulls = Nulls.SKIP) var inJail: Boolean = false,
+    @JsonSetter(nulls = Nulls.SKIP) var jailTurns: Int = 0,
+    @JsonSetter(nulls = Nulls.SKIP) var getOutOfJailCards: Int = 0,
+    @JsonSetter(nulls = Nulls.SKIP) val ownedPropertyIds: MutableList<Int> = mutableListOf()
 ) {
     /** Returns true if the player is bankrupt (no money and no properties). */
     fun isBankrupt(): Boolean = money <= 0 && ownedPropertyIds.isEmpty()
